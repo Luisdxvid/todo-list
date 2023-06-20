84efe3c2909c824d86a707a60b7f021b625d3371
Luis <luisrodriguezortigoza@gmail.com>
2023-06-20 15:49:20 -0400

    Adding icons to remove and edit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
-import { RiCloseCircleLine } from "react-icons/ri";
+import { CiCircleRemove } from "react-icons/ci";
+import { TiEdit } from "react-icons/ti";
 
-function Todo() {
+function Todo({ todos, completeTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
@@ -17,8 +18,11 @@ function Todo() {
         {todo.text}
       </div>
       <div className="icons">
-        <AiOutlineCloseCircle />
-        <TiEdit />
+        <CiCircleRemove
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit onClick={() => removeTodo(todo.id)} className="delete-icon" />
       </div>
     </div>
   ));
