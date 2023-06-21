c9554042925197ae5a016c683af35f27842ad007
Luis <luisrodriguezortigoza@gmail.com>
2023-06-21 14:16:54 -0400

    Queries for responsive and Clock (not functional yet)

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,63 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import TodoForm from "./TodoForm";
+// import Todo from "./Todo";
+
+// function TodoList() {
+//   const [todos, setTodos] = useState([]);
+
+//   const addTodo = (todo) => {
+//     if (!todo.text || /^\$*$/.test(todo.text)) {
+//       return;
+//     }
+
+//     const newTodos = [todo, ...todos];
+
+//     setTodos(newTodos);
+//   };
+
+//   const updateTodo = (todoId, newValue) => {
+//     if (!newValue.text || /^\$*$/.test(newValue.text)) {
+//       return;
+//     }
+
+//     setTodos((prev) =>
+//       prev.map((item) => (item.id === todoId ? newValue : item))
+//     );
+//   };
+
+//   const removeTodo = (id) => {
+//     const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+//     setTodos(removeArr);
+//   };
+
+//   const completeTodo = (id) => {
+//     let updatedTodos = todos.map((todo) => {
+//       if (todo.id === id) {
+//         todo.isComplete = !todo.isComplete;
+//       }
+//       return todo;
+//     });
+//     setTodos(updatedTodos);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Que tienes para hoy?</h1>
+//       <TodoForm onSubmit={addTodo} />
+//       <Todo
+//         todos={todos}
+//         completeTodo={completeTodo}
+//         removeTodo={removeTodo}
+//         updateTodo={updateTodo}
+//       />
+//     </div>
+//   );
+// }
+
+// export default TodoList;
+
+import React, { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 
